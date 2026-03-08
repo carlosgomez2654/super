21493748ed34c2b8914750bf80e56a74d64fa759
Carlos Gomez <carlosgomez2654@gmail.com>
2026-03-08 22:10:47 +1000

    arreglo app y gitignore

--- proyecto/app.py
+++ proyecto/app.py
@@ -429,5 +429,5 @@ def logout():
 # RUN
 # ==========================
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
